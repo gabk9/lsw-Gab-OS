ea146d8071a670bda28a67563b3bd1b7d1439fef
Gabriel Oliveira Miranda <giane.ga2010@gmail.com>
2026-02-17 11:04:58 -0300

    edited the nonull attribute, cuz it does not work with MINGW

--- libs/s_math.h
+++ libs/s_math.h
@@ -67,7 +67,6 @@ double s_log10(char *operation)__attribute__((nonnull));
 double s_scale(char *operation)__attribute__((nonnull));
 double s_pounds(char *operation)__attribute__((nonnull));
 double s_randInt(char *operation)__attribute__((nonnull));
-double h_atof(const char *str, bool mathlib)__nonnull((1));
 char *find_top_level_comma(char *s)__attribute__((nonnull));
 double s_randFloat(char *operation)__attribute__((nonnull));
 int64_t parseBinToInt(const char *str)__attribute__((nonnull));
@@ -76,6 +75,7 @@ __attribute__((unused))
 bool parentheses_balanced(const char *s)__attribute__((nonnull));
 
 uint16_t count_top_level_commas(const char *s)__attribute__((nonnull));
+double h_atof(const char *str, bool mathlib)__attribute__((nonnull(1)));
 double s_fabs_or_abs(char *operation, bool enable_single_point)__attribute__((nonnull));
 char *functionHandler(char *operation, const char *function)__attribute__((warn_unused_result));
 
