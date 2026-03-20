0a25167ff2574901eff08ff9b4efbe8a3c3d05bc
Gabriel Oliveira Miranda <giane.ga2010@gmail.com>
2026-03-20 15:41:14 -0300

    - edited a macro value

--- libs/utils.c
+++ libs/utils.c
@@ -3,7 +3,7 @@
 #include "types.h"
 
 #define PROJ_LINES_APPROX 9700
-#define PROJ_SIZE_APPROX_BYTES 293000
+#define PROJ_SIZE_APPROX_BYTES 292000
 
 #define PATH_MAIN_C "./main.c"
 #define PATH_UTILS_C "./libs/utils.c"
